e1eec1866a2ffd0a79b44369f9975ed65175a8a7
Chris Kelner <chris.kelner@gmail.com>
2023-06-26 10:47:57 -0700

    rename controller

--- POCs/containers/java/multi-line-logging/src/main/java/com/kelner/multiline/springboot/controller/MainController.java
+++ POCs/containers/java/multi-line-logging/src/main/java/com/kelner/multiline/springboot/controller/MainController.java
@@ -3,12 +3,14 @@ package com.kelner.multiline.springboot.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-public class HelloController {
+public class MainController {
 
-    @GetMapping("/hello")
-    public ResponseEntity<String> sayHello() {
+    @GetMapping("/")
+    public ResponseEntity<String> index() {
 
         return ResponseEntity.ok("Hello World");
     }
