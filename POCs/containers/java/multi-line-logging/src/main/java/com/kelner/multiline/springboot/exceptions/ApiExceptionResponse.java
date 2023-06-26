9f7557fe57f555e3c4f8e6042433de728f75962e
Chris Kelner <chris.kelner@gmail.com>
2023-06-26 10:19:21 -0700

    java project

---
+++ POCs/containers/java/multi-line-logging/src/main/java/com/kelner/multiline/springboot/exceptions/ApiExceptionResponse.java
@@ -0,0 +1,23 @@
+package com.kelner.multiline.springboot.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiExceptionResponse {
+
+    private String message;
+
+    private HttpStatus status;
+
+    private LocalDateTime time;
+
+}
