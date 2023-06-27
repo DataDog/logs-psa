6e5c2b498103735745caae8c2919f0736a54da3c
Chris Kelner <chris.kelner@gmail.com>
2023-06-26 23:17:26 -0700

    comment

--- POCs/containers/java/multi-line-logging/build-deploy.sh
+++ POCs/containers/java/multi-line-logging/build-deploy.sh
@@ -18,6 +18,8 @@ docker build . -t multiline-poc:$fakever
 
 minikube image load multiline-poc:$fakever
 
+# un-comment last set if you do not want to use log annotations
 helm upgrade multiline-poc ./k8s/multiline-poc/ --install \
     -f ./k8s/multiline-poc/values.yaml \
-    --set-string image.tag="${fakever}"
+    --set-string image.tag="${fakever}" \
+    #--set agents.containers.agent.envDict.DD_LOGS_CONFIG_AUTO_MULTI_LINE_EXTRA_PATTERNS='(..@timestamp|\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2})
