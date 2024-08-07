f37ba03f90cf6dc35a068617c65a538b333e01ac
Chris Kelner <chris.kelner@gmail.com>
2024-08-06 17:00:31 -0700

    breaks out scenario to function

--- POCs/XML-logging/Program.cs
+++ POCs/XML-logging/Program.cs
@@ -34,41 +34,8 @@ namespace ConsoleExample
                         loggingBuilder.AddNLog(config);
                     }).BuildServiceProvider();
 
-                var nLogLogger = NLog.LogManager.GetCurrentClassLogger();
-                XmlDocument doc = new XmlDocument();
-                doc.Load("sample.xml");
-                if( doc != null && doc.GetElementById("bk101") != null)
-                {
-                    XmlElement? bk101 = doc.GetElementById("bk101");
+                scenarioOne();
 
-                    if (bk101 != null && bk101.HasChildNodes)
-                    {
-                        XmlNodeList bk101ChildNodes = bk101.ChildNodes;
-
-                        var logEvent = new LogEventInfo(NLog.LogLevel.Info, "XmlLogs", null);
-
-                        /* we don't suggest you log every attribute of the XML file,
-                        but this is an example of how you can attach attributes to the log */
-                        foreach (XmlNode node in bk101ChildNodes)
-                        {
-                            logEvent.Properties[node.Name] = node.InnerText;
-                        }
-                        /* and here's a single arbitrary attribute being added, but you could imagine
-                        that it was derived from an XML attribute or element */
-                        logEvent.Properties["arbitrary"] = "arbitrary value";
-
-                        logEvent.Message = "Logging from XML - put something useful here, even poor man's debugging: 'Made it this far 1'";
-
-                        nLogLogger.Log(logEvent);
-                    }
-                    else
-                    {
-                        nLogLogger.Error("bk101 not found in sample.xml");
-                    }
-                } else
-                {
-                    throw new Exception("Document element is null");
-                }
             }
             catch (Exception ex)
             {
@@ -80,6 +47,48 @@ namespace ConsoleExample
             {
                 // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
                 LogManager.Shutdown();
+            }
+        }
+
+        /*
+            Scenario One - picks off key:values from an XML file and logs them
+            as attributes in the log event.
+        */
+        static void scenarioOne() {
+            var nLogLogger = NLog.LogManager.GetCurrentClassLogger();
+            XmlDocument doc = new XmlDocument();
+            doc.Load("sample.xml");
+            if( doc != null && doc.GetElementById("bk101") != null)
+            {
+                XmlElement? bk101 = doc.GetElementById("bk101");
+
+                if (bk101 != null && bk101.HasChildNodes)
+                {
+                    XmlNodeList bk101ChildNodes = bk101.ChildNodes;
+
+                    var logEvent = new LogEventInfo(NLog.LogLevel.Info, "XmlLogs", null);
+
+                    /* we don't suggest you log every attribute of the XML file,
+                    but this is an example of how you can attach attributes to the log */
+                    foreach (XmlNode node in bk101ChildNodes)
+                    {
+                        logEvent.Properties[node.Name] = node.InnerText;
+                    }
+                    /* and here's a single arbitrary attribute being added, but you could imagine
+                    that it was derived from an XML attribute or element */
+                    logEvent.Properties["arbitrary"] = "arbitrary value";
+
+                    logEvent.Message = "Logging from XML - put something useful here, even poor man's debugging: 'Made it this far 1'";
+
+                    nLogLogger.Log(logEvent);
+                }
+                else
+                {
+                    nLogLogger.Error("bk101 not found in sample.xml");
+                }
+            } else
+            {
+                throw new Exception("Document element is null");
             }
         }
     }
