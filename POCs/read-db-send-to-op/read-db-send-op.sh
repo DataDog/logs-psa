e4d6db61a3ae03da5933512207aa07487f36481d
Chris Kelner <chris.kelner@gmail.com>
2025-05-13 16:13:04 -0700

    count key

--- POCs/read-db-send-to-op/read-db-send-op.sh
+++ POCs/read-db-send-to-op/read-db-send-op.sh
@@ -5,6 +5,7 @@ DB_HOST="localhost"
 DB_USER="your_user"
 DB_PASS="your_password"
 DB_NAME="your_database"
+COUNT_KEY="event_id"
 
 # Get the last event ID from the file
 if [[ ! -f event.txt ]]; then
