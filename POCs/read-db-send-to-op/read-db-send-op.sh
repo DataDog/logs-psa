a04be2579a1fb04354a8c4c216fced7d0ccc3649
Chris Kelner <chris.kelner@gmail.com>
2025-05-13 16:13:18 -0700

    update query

--- POCs/read-db-send-to-op/read-db-send-op.sh
+++ POCs/read-db-send-to-op/read-db-send-op.sh
@@ -14,7 +14,7 @@ fi
 LAST_EVENT=$(< event.txt)
 
 # Query to run
-QUERY="SELECT id, log, transaction_id FROM logs;"
+QUERY="SELECT id, log, transaction_id, event_id FROM logs WHERE $COUNT_KEY > $LAST_EVENT ORDER BY $COUNT_KEY LIMIT 1000;"
 
 # URL to send data to
 ENDPOINT_URL="https://your.op.endpoint/"
