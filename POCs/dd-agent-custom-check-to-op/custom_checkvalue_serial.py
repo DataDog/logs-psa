671a1e6e0286648b548ee6504f08912756440d51
Chris Kelner <chris.kelner@gmail.com>
2025-05-27 17:02:01 -0700

    simplify logging, no need to loop

--- POCs/dd-agent-custom-check-to-op/custom_checkvalue_serial.py
+++ POCs/dd-agent-custom-check-to-op/custom_checkvalue_serial.py
@@ -8,24 +8,16 @@ class BashCheck(AgentCheck):
         TCP_HOST = '127.0.0.1'  # local dd agent
         TCP_PORT = 10518        # tcp listener port
 
-        all_logs = []
-
-        # for loop to loop 30 times to collect logs serially
-        # in a real scenario, you wouldn't loop and you'd call different
-        # bash scripts or commands
+        # for loop to loop 30 times to collect logs serially.
+        # In a real scenario, you wouldn't loop and you'd call different
+        # bash scripts or commands in sequence in this check, but for the
+        # purposes of proof-of-concept, this should behave the same way
         for i in range(30):
             try:
                 logs = get_subprocess_output(["bash", "/opt/custom_bash.sh"], self.log, raise_on_empty_output=True)
-                all_logs += logs.splitlines()
+                # Create a TCP socket
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.connect((TCP_HOST, TCP_PORT))
+                    s.sendall(logs[0].encode('utf-8'))
             except Exception as e:
-                self.log.error(f"Error collecting logs: {e}")
-
-        for log in all_logs:
-            if log:
-                try:
-                    # Create a TCP socket
-                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                        s.connect((TCP_HOST, TCP_PORT))
-                        s.sendall(log.encode('utf-8'))
-                except Exception as e:
-                    print(f"Error sending '{log}': {e}")
+                print(f"Error collecting logs: {e}")
